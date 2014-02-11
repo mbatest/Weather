070e630de0b60b5e791f7f142d53c9f1160af24a
MB <bauderon@labri.fr>
2014-02-11 17:29:24 +0100

    Version 1 - 11/02/2014

---
+++ Weather/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.listPluie = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.nouveauToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.ouvrirToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.enregistrerToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.imprimerToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.couperToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.copierToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.collerToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ToolStripButton = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(369, 416);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(869, 416);
+            this.splitContainer1.SplitterDistance = 369;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.listPluie);
+            this.splitContainer2.Size = new System.Drawing.Size(496, 416);
+            this.splitContainer2.SplitterDistance = 153;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // listPluie
+            // 
+            this.listPluie.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listPluie.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listPluie.Location = new System.Drawing.Point(0, 0);
+            this.listPluie.Name = "listPluie";
+            this.listPluie.Size = new System.Drawing.Size(496, 153);
+            this.listPluie.TabIndex = 0;
+            this.listPluie.UseCompatibleStateImageBehavior = false;
+            this.listPluie.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mois";
+            this.columnHeader1.Width = 118;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Précipitations (cm)";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader2.Width = 76;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.toolStrip1);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.splitContainer1);
+            this.splitContainer3.Size = new System.Drawing.Size(869, 445);
+            this.splitContainer3.SplitterDistance = 25;
+            this.splitContainer3.TabIndex = 3;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nouveauToolStripButton,
+            this.ouvrirToolStripButton,
+            this.enregistrerToolStripButton,
+            this.imprimerToolStripButton,
+            this.toolStripSeparator,
+            this.couperToolStripButton,
+            this.copierToolStripButton,
+            this.collerToolStripButton,
+            this.toolStripSeparator1,
+            this.ToolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(869, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // nouveauToolStripButton
+            // 
+            this.nouveauToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.nouveauToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("nouveauToolStripButton.Image")));
+            this.nouveauToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.nouveauToolStripButton.Name = "nouveauToolStripButton";
+            this.nouveauToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.nouveauToolStripButton.Text = "&Nouveau";
+            // 
+            // ouvrirToolStripButton
+            // 
+            this.ouvrirToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ouvrirToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("ouvrirToolStripButton.Image")));
+            this.ouvrirToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ouvrirToolStripButton.Name = "ouvrirToolStripButton";
+            this.ouvrirToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.ouvrirToolStripButton.Text = "&Ouvrir";
+            this.ouvrirToolStripButton.Click += new System.EventHandler(this.ouvrirToolStripButton_Click);
+            // 
+            // enregistrerToolStripButton
+            // 
+            this.enregistrerToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.enregistrerToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("enregistrerToolStripButton.Image")));
+            this.enregistrerToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.enregistrerToolStripButton.Name = "enregistrerToolStripButton";
+            this.enregistrerToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.enregistrerToolStripButton.Text = "&Enregistrer";
+            // 
+            // imprimerToolStripButton
+            // 
+            this.imprimerToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.imprimerToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("imprimerToolStripButton.Image")));
+            this.imprimerToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.imprimerToolStripButton.Name = "imprimerToolStripButton";
+            this.imprimerToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.imprimerToolStripButton.Text = "&Imprimer";
+            // 
+            // toolStripSeparator
+            // 
+            this.toolStripSeparator.Name = "toolStripSeparator";
+            this.toolStripSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // couperToolStripButton
+            // 
+            this.couperToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.couperToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("couperToolStripButton.Image")));
+            this.couperToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.couperToolStripButton.Name = "couperToolStripButton";
+            this.couperToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.couperToolStripButton.Text = "C&ouper";
+            // 
+            // copierToolStripButton
+            // 
+            this.copierToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.copierToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("copierToolStripButton.Image")));
+            this.copierToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.copierToolStripButton.Name = "copierToolStripButton";
+            this.copierToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.copierToolStripButton.Text = "Co&pier";
+            // 
+            // collerToolStripButton
+            // 
+            this.collerToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.collerToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("collerToolStripButton.Image")));
+            this.collerToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.collerToolStripButton.Name = "collerToolStripButton";
+            this.collerToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.collerToolStripButton.Text = "Co&ller";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // ToolStripButton
+            // 
+            this.ToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("ToolStripButton.Image")));
+            this.ToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ToolStripButton.Name = "ToolStripButton";
+            this.ToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.ToolStripButton.Text = "&?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 445);
+            this.Controls.Add(this.splitContainer3);
+            this.Name = "Form1";
+            this.Text = "Weather station";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel1.PerformLayout();
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ListView listPluie;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton nouveauToolStripButton;
+        private System.Windows.Forms.ToolStripButton ouvrirToolStripButton;
+        private System.Windows.Forms.ToolStripButton enregistrerToolStripButton;
+        private System.Windows.Forms.ToolStripButton imprimerToolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+        private System.Windows.Forms.ToolStripButton couperToolStripButton;
+        private System.Windows.Forms.ToolStripButton copierToolStripButton;
+        private System.Windows.Forms.ToolStripButton collerToolStripButton;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton ToolStripButton;
+    }
+}
+
---
+++ Weather/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weather
+{
+    public partial class Form1 : Form
+    {
+        string basePath = @"E:\Mes Documents\HeavyWeather\History\";
+        public SortedList<string, Data> données = new SortedList<string, Data>();
+        public SortedList<DateTime, Data> donnéesDate = new SortedList<DateTime, Data>();
+        public List<Data> dats = new List<Data>();
+        public List<Pluie> pluviométrie = new List<Pluie>();
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.Columns.Add("Numéro");
+            listView1.Columns.Add("Date", 100);
+            listView1.Columns.Add("Pression", 50, HorizontalAlignment.Right);
+            listView1.Columns.Add("Température", 50, HorizontalAlignment.Right);
+            listView1.Columns.Add("Humidité", 50, HorizontalAlignment.Right);
+            listView1.Columns.Add("Rafales", 50, HorizontalAlignment.Right);
+        }
+        private void ouvrirToolStripButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.InitialDirectory = basePath;
+            opf.Filter = "History (*.dat)|*.dat";
+            if (opf.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FileStream sr = new FileStream(opf.FileName, FileMode.Open);
+                int i = 0;
+                while (sr.Position < sr.Length)
+                {
+                    Data d = new Data(sr);
+                    string key = (d.date.ToShortDateString() + " " + d.date.ToShortTimeString()).Replace(".", "/");
+                    données.Add(key, d);
+                    donnéesDate.Add(d.date, d);
+                    i++;
+                }
+                sr.Close();
+                foreach (Data d in donnéesDate.Values)
+                    dats.Add(d);
+                int j = 6;
+                int mois = dats[j].date.Month;
+                Pluie p = new Pluie(mois);
+
+                while (j < dats.Count)
+                {
+                    int current = dats[j].date.Month;
+                    while (current == mois)
+                    {
+                        p.haut += dats[j].pluviométrieRécente;
+                        j++;
+                        if (j == dats.Count)
+                            break;
+                        current = dats[j].date.Month;
+                    }
+                    p.haut /= 10;
+                    pluviométrie.Add(p);
+                    mois = current;
+                    p = new Pluie(mois);
+                }
+                i = 0;
+                foreach (Data d in dats)
+                {
+                    ListViewItem lw = new ListViewItem(i.ToString());
+                    listView1.Items.Add(lw);
+                    lw.SubItems.Add(d.date.ToShortDateString() + " " + d.date.ToShortTimeString());
+                    lw.SubItems.Add(d.pressionRelative.ToString("f2"));
+                    lw.SubItems.Add(d.températureExtérieure.ToString("f2"));
+                    if (d.humiditéExtérieure < 100)
+                        lw.SubItems.Add(d.humiditéExtérieure.ToString("f2"));
+                    else
+                        lw.SubItems.Add("---");
+
+                    if (d.directWind != 16)
+                        lw.SubItems.Add((d.VitesseRafale).ToString("f2"));
+                    else
+                        lw.SubItems.Add("---");
+
+                    i++;
+                }
+                foreach (Pluie pl in pluviométrie)
+                {
+                    ListViewItem lw = new ListViewItem(pl.Mois);
+                    lw.SubItems.Add(pl.haut.ToString("f2"));
+                    listPluie.Items.Add(lw);
+                }
+            }
+
+        }
+
+
+     }
+
+    public class Pluie
+    {
+
+        private int mois;
+
+        public string Mois
+        {
+            get
+            {
+                string[] mo = new string[] { "", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre" };
+                return mo[mois]; }
+        }
+        public float haut;
+        public Pluie(int mois) { this.mois = mois; }
+        public override string ToString()
+        {
+            return mois.ToString() + " " + haut.ToString();
+        }
+    }
+    public class Data
+    {
+        /*Each row of data is stored in 56 byte chunks starting from the beginning of the file (no header).
+
+ ROW
+ OFFSET Type Name Unit
+------ --------- ---------------- -----
+00 Double [8] Timestamp days from 12/30/1899 00:00:00 (GMT) 
+ 08 Float [4] Abs Pressure hectopascals (millibars)
+ 12 Float [4] Relative Pressure hectopascals (millibars)
+ 16 Float [4] Wind Speed meters/second
+ 20 ULong [4] Wind Direction see below
+ 24 Float [4] Wind Gust meters/second
+ 28 Float [4] Total Rainfall millimeters
+ 32 Float [4] New Rainfall millimeters
+ 36 Float [4] Indoor Temp celsius
+ 40 Float [4] Outdoor Temp celsius
+ 44 Float [4] Indoor Humidity %
+ 48 Float [4] Outdoor Humidity %
+ 52 ULong [4] unknown - (Value is always 0)
+ 
+         */
+        /*Temp_Rosee:=
+      (237.7*((17.27*Temp)/(237.7+Temp)+ln(Hum)))/(17.27-((17.27*Temp)/(237.7+Temp)+ln(Hum))); 
+      (où Temp est la température extérieure en °C et Hum le taux d'humidité extérieure (attention entre 0 et 1))
+      Pour la Température Ressentie le calcul est :
+      Temp_Ress:= 13.12+0.6215*Temp+(0.3965*Temp-11.37)*(Vent^(0.16));
+      (où Temp est la Température extérieure en °C et Vent la vitesse du vent en km/h))*/
+        string[] wind = new string[] { "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSO", "SO", "OSO", "O", "ONO", "NO", "NNO", "---" };
+        public long position;
+        public byte[] dat = new byte[8];
+        public float pressionAbsolue ;
+        public float pressionRelative ;
+        public float vitesseVent ;
+        public int directWind ;
+        private float vitesseRafale;
+        public float VitesseRafale
+        {
+            get { return vitesseRafale * 3.6f; }
+        }
+        public float pluviométrieTotale ;
+        public float pluviométrieRécente ;
+        public float températureIntérieure ;
+        public float températureExtérieure ;
+        public float humiditéIntérieure ;
+        public float humiditéExtérieure ;
+        public byte[] w = new byte[4];
+        public double db;
+        public DateTime date;
+        public string chaineDate;
+        public string DirectionVent
+        {
+            get
+            {
+                return wind[directWind];
+            }
+        }
+        public Data(FileStream fs)
+        {
+            position = fs.Position;
+            fs.Read(dat, 0, dat.Length);
+            pressionAbsolue = ReadFloat(fs);
+            pressionRelative = ReadFloat(fs);
+            vitesseVent = ReadFloat(fs);
+            fs.Read(w, 0, w.Length);
+            directWind = System.BitConverter.ToInt32(w, 0);
+            vitesseRafale = ReadFloat(fs); 
+            pluviométrieTotale = ReadFloat(fs);
+            pluviométrieRécente = ReadFloat(fs);
+            températureIntérieure = ReadFloat(fs);
+            températureExtérieure = ReadFloat(fs);
+            humiditéIntérieure = ReadFloat(fs);
+            humiditéExtérieure = ReadFloat(fs);
+            db = System.BitConverter.ToDouble(dat, 0);// *86400 * 10000000;
+            //           DateTime startDate = new DateTime(1900, 1,1, 0, 0, 0);
+            DateTime startDate = new DateTime(1899, 12, 30, 0, 0, 0);
+            TimeSpan ts = timeSpan(db * 86400);
+            date = startDate + ts;
+            fs.Read(w, 0, w.Length);
+
+        }
+        private TimeSpan timeSpan(double db)
+        {
+            TimeSpan ts = new TimeSpan((long)(db * 10000000));
+            return ts;
+
+        }
+
+        public Data(string line)
+        {
+            line = line.Replace("\"", "");
+            string[] data = line.Split(';');
+            chaineDate = data[1];
+        }
+        float ReadFloat(FileStream fs)
+        {
+            byte[] w = new byte[4];
+            fs.Read(w, 0, w.Length);
+            return System.BitConverter.ToSingle(w, 0);
+
+        }
+        public override string ToString()
+        {
+            return date.ToShortDateString() + " " + date.ToShortTimeString();
+            //+" " + dat[7].ToString("x") + dat[6].ToString("x") + dat[5].ToString("x") + " " + dat[4].ToString("x") + dat[3].ToString("x") + dat[2].ToString("x") + dat[1].ToString("x") + dat[0].ToString("x");
+        }
+    }
+
+}
